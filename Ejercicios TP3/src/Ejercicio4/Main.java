5d923055c786c5199e2a7acd6326c3058ccbfdd8
Uriel <urielvera47@gmail.com>
2024-04-29 16:12:48 -0300

    Se Agregó Ejercicios TP3

--- Ejercicios TP3/src/Ejercicio4/Main.java
+++ Ejercicios TP3/src/Ejercicio4/Main.java
@@ -1,5 +1,6 @@
 package Ejercicio4;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
@@ -16,78 +17,88 @@ public class Main {
 
         //Pedir datos
         while (true) {
-            System.out.println("Bienvenido al sistema ¿Qué acción desea realizar?");
-            System.out.println("1. Agregar alumno");
-            System.out.println("2. Mostrar lista de alumnos");
-            System.out.println("3. Calcular promedio de notas");
-            System.out.println("4. Buscar alumno");
-            System.out.println("5. Salir");
+            try {
+                System.out.println("Bienvenido al sistema ¿Qué acción desea realizar?");
+                System.out.println("1. Agregar alumno");
+                System.out.println("2. Mostrar lista de alumnos");
+                System.out.println("3. Calcular promedio de notas");
+                System.out.println("4. Buscar alumno");
+                System.out.println("5. Salir");
 
-            int opcion = in.nextInt();
+                int opcion = in.nextInt();
 
-            switch (opcion) {
-                case 1:
-                    System.out.println("Ingrese el nombre del alumno");
-                    String name = in.next();
+                switch (opcion) {
+                    case 1:
+                        System.out.println("Ingrese el nombre del alumno");
+                        String name = in.next();
 
-                    System.out.println("Ingrese la edad del alumno");
-                    int age = in.nextInt();
-
-                    System.out.println("Ingrese la nota del alumno");
-                    double grade = in.nextDouble();
-
-                    Alumno student = new Alumno(name, age, grade);
-                    students.add(student);
-                    System.out.println("Alumno agregado");
-                    break;
-
-                case 2:
-                    System.out.println("Lista de alumnos:");
-                    for (Alumno s : students) {
-                        System.out.println("Nombre: " + s.getName() + ", Edad: " + s.getAge() + ", Nota: " + s.getNota());
-                    }
-                    break;
-
-                case 3:
-                    double sum = 0;
-                    double average = 0;
-                    double highestGrade = 0;
-                    Alumno studentWithHighestGrade = null;
-                    System.out.println("Promedio de notas");
-                    for (Alumno s : students) {
-                        sum += s.getNota();
-                    }
-                    average = sum / students.size();
-                    System.out.println("El promedio de notas de los alumnos es de: " + average);
-
-                    for (Alumno s : students) {
-                        if (s.getNota() > highestGrade) {
-                            highestGrade = s.getNota();
-                            studentWithHighestGrade = s;
+                        System.out.println("Ingrese la edad del alumno");
+                        int age = in.nextInt();
+                        if (age == 0) {
+                            throw new ArithmeticException("La edad del alumno no puede ser 0");
                         }
-                    }
-                    System.out.println("El alumno con mayor nota es: " + studentWithHighestGrade.getName() + " con una nota de: " + highestGrade);
-                    break;
 
-                case 4:
-                    System.out.println("Buscar alumno");
-                    System.out.println("Ingrese el nombre del alumno a buscar");
-                    String nameToSearch = in.next();
-                    for (Alumno s : students) {
-                        if (s.buscarAlumno(nameToSearch)) {
-                            System.out.println("Alumno encontrado");
+                        System.out.println("Ingrese la nota del alumno");
+                        double grade = in.nextDouble();
+
+                        Alumno student = new Alumno(name, age, grade);
+                        students.add(student);
+                        System.out.println("Alumno agregado");
+                        break;
+
+                    case 2:
+                        System.out.println("Lista de alumnos:");
+                        for (Alumno s : students) {
                             System.out.println("Nombre: " + s.getName() + ", Edad: " + s.getAge() + ", Nota: " + s.getNota());
                         }
-                    }
-                    break;
+                        break;
 
-                case 5:
-                    System.out.println("Saliendo del programa...");
-                    System.exit(0);
-                    break;
+                    case 3:
+                        double sum = 0;
+                        double average = 0;
+                        double highestGrade = 0;
+                        Alumno studentWithHighestGrade = null;
+                        System.out.println("Promedio de notas");
+                        for (Alumno s : students) {
+                            sum += s.getNota();
+                        }
+                        average = sum / students.size();
+                        System.out.println("El promedio de notas de los alumnos es de: " + average);
 
-                default:
-                    System.out.println("La opción ingresada no es válida");
+                        for (Alumno s : students) {
+                            if (s.getNota() > highestGrade) {
+                                highestGrade = s.getNota();
+                                studentWithHighestGrade = s;
+                            }
+                        }
+                        System.out.println("El alumno con mayor nota es: " + studentWithHighestGrade.getName() + " con una nota de: " + highestGrade);
+                        break;
+
+                    case 4:
+                        System.out.println("Buscar alumno");
+                        System.out.println("Ingrese el nombre del alumno a buscar");
+                        String nameToSearch = in.next();
+                        for (Alumno s : students) {
+                            if (s.buscarAlumno(nameToSearch)) {
+                                System.out.println("Alumno encontrado");
+                                System.out.println("Nombre: " + s.getName() + ", Edad: " + s.getAge() + ", Nota: " + s.getNota());
+                            }
+                        }
+                        break;
+
+                    case 5:
+                        System.out.println("Saliendo del programa...");
+                        System.exit(0);
+                        break;
+
+                    default:
+                        System.out.println("La opción ingresada no es válida");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Por favor, ingrese un número válido");
+                in.nextLine();
+            } catch (ArithmeticException e) {
+                System.out.println("Ha ocurrido un error " + e.getMessage());
             }
         }
     }
--- Ejercicios TP3/src/Ejercicio5/Main.java
+++ Ejercicios TP3/src/Ejercicio5/Main.java
@@ -1,5 +1,6 @@
 package Ejercicio5;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.ArrayList;
 
@@ -16,54 +17,64 @@ public class Main {
 
         //Pedir datos
         while (true) {
-            System.out.println("Bienvenido al sistema ¿Qué acción desea realizar?");
-            System.out.println("1. Agregar nueva tarea");
-            System.out.println("2. Mostrar todas las tareas");
-            System.out.println("3. Eliminar una tarea");
-            System.out.println("4. Salir");
+            try {
+                System.out.println("Bienvenido al sistema ¿Qué acción desea realizar?");
+                System.out.println("1. Agregar nueva tarea");
+                System.out.println("2. Mostrar todas las tareas");
+                System.out.println("3. Eliminar una tarea");
+                System.out.println("4. Salir");
 
-            int option = in.nextInt();
-            System.out.println("-----------------------------");
+                int option = in.nextInt();
+                System.out.println("-----------------------------");
 
-            switch (option) {
-                case 1:
-                    System.out.println("Ingrese la descripción de la tarea:");
-                    in.nextLine();
-                    String descripcion = in.nextLine();
+                switch (option) {
+                    case 1:
+                        System.out.println("Ingrese la descripción de la tarea:");
+                        in.nextLine();
+                        String descripcion = in.nextLine();
 
-                    System.out.println("Ingrese la prioridad de la tarea");
-                    int prioridad = in.nextInt();
+                        System.out.println("Ingrese la prioridad de la tarea");
+                        int prioridad = in.nextInt();
+                        if (prioridad == 0) {
+                            throw new ArithmeticException("La prioridad de la tarea no puede ser 0");
+                        }
 
-                    Tarea tarea = new Tarea(descripcion, prioridad);
-                    lt1.agregarTarea(tarea);
-                    System.out.println("Tarea agregada");
-                    System.out.println("-----------------------------");
-                    break;
+                        Tarea tarea = new Tarea(descripcion, prioridad);
+                        lt1.agregarTarea(tarea);
+                        System.out.println("Tarea agregada");
+                        System.out.println("-----------------------------");
+                        break;
 
-                case 2:
-                    System.out.println("Lista de tareas:");
-                    for (Tarea t : lt1) {
-                        System.out.println("Descripción: " + t.getDescription() + ", Prioridad: " + t.getPriority());
-                    }
-                    System.out.println("-----------------------------");
-                    break;
+                    case 2:
+                        System.out.println("Lista de tareas:");
+                        for (Tarea t : lt1) {
+                            System.out.println("Descripción: " + t.getDescription() + ", Prioridad: " + t.getPriority());
+                        }
+                        System.out.println("-----------------------------");
+                        break;
 
-                case 3:
-                    in.nextLine();
-                    System.out.println("Ingrese la descripción de la tarea que desea eliminar");
-                    String descripcionEliminar = in.nextLine();
-                    lt1.eliminarTarea(descripcionEliminar);
-                    System.out.println("Tarea eliminada");
-                    System.out.println("-----------------------------");
-                    break;
+                    case 3:
+                        in.nextLine();
+                        System.out.println("Ingrese la descripción de la tarea que desea eliminar");
+                        String descripcionEliminar = in.nextLine();
+                        lt1.eliminarTarea(descripcionEliminar);
+                        System.out.println("Tarea eliminada");
+                        System.out.println("-----------------------------");
+                        break;
 
-                case 4:
-                    System.out.println("Saliendo del programa...");
-                    System.exit(0);
-                    break;
+                    case 4:
+                        System.out.println("Saliendo del programa...");
+                        System.exit(0);
+                        break;
 
-                default:
-                    System.out.println("La opción ingresada no es válida");
+                    default:
+                        System.out.println("La opción ingresada no es válida");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Por favor, ingrese un número válido");
+                in.nextLine();
+            } catch (ArithmeticException e) {
+                System.out.println("Ha ocurrido un error " + e.getMessage());
             }
         }
     }
